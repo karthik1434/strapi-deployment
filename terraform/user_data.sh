043f99ad44d7a3cb6d44ab0e3fe65d1bafa30963
Your Name <your-email@example.com>
2025-04-19 19:31:41 +0530

    karthik

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
-set -ex  # Enable debugging
-
-# Redirect all output to a log file
-exec > >(tee /var/log/user-data.log) 2>&1
-
-echo "=== Starting user-data ==="
-date
 
 # Install dependencies
 apt-get update
-apt-get install -y docker.io awscli
+apt-get install -y docker.io
 
-echo "=== AWS CLI Version ==="
-aws --version
 
-echo "=== Trying ECR Login ==="
-aws ecr get-login-password --region us-east-1 | \
-  docker login --username AWS --password-stdin 118273046134.dkr.ecr.us-east-1.amazonaws.com
-
-echo "=== Docker Info ==="
-docker info
-
-echo "=== Pulling Image ==="
 docker pull 118273046134.dkr.ecr.us-east-1.amazonaws.com/strapi-app-karthik:latest
-
-echo "=== Running Container ==="
 docker run -d \
   -p 1337:1337 \
   --name strapi-app-karthik \
-  --restart unless-stopped \
   118273046134.dkr.ecr.us-east-1.amazonaws.com/strapi-app-karthik:latest
-
-echo "=== Completed successfully ==="
-date
