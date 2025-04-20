77080c6eb1d069ac0b99c899cee7ad604dc86350
Your Name <your-email@example.com>
2025-04-20 09:06:28 +0530

    final

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Install dependencies
-apt-get update
+# Install Docker and AWS CLI
+apt-get update -y
 apt-get install -y docker.io awscli
-# Pull and run Strapi container
+
+# Start Docker
+systemctl start docker
+systemctl enable docker
+
+# Wait to ensure EC2 IAM role credentials are available
+sleep 30
+
+# Authenticate with ECR
+aws ecr get-login-password --region us-east-1 | \
+docker login --username AWS --password-stdin 118273046134.dkr.ecr.us-east-1.amazonaws.com
+
+# Pull and run the image
 docker pull 118273046134.dkr.ecr.us-east-1.amazonaws.com/strapi-app-karthik:latest
+
 docker run -d \
   -p 1337:1337 \
   --name strapi-app-karthik \
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "image_tag" {
