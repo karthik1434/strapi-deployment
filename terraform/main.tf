b106f2c41486ffcd52dd6a82ec46174d57287a8f
Your Name <your-email@example.com>
2025-04-20 06:52:16 +0530

    final

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "strapi" {
 }
 
 resource "aws_security_group" "strapi" {
-  name = "strapi-security-group-karthik22"
+  name = "strapi-security-group-karthik23"
 
   ingress {
     from_port   = 1337
