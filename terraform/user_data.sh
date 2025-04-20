d9edc848ba5560025ea80eece875cb5ed2db9c72
Your Name <your-email@example.com>
2025-04-20 22:24:14 +0530

    final

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -10,13 +10,6 @@ systemctl enable docker
 
 sleep 20
 
-# Wait to ensure EC2 IAM role credentials are available
-# Configure AWS credentials
-export AWS_ACCESS_KEY_ID="${var.aws_access_key_id}"
-export AWS_SECRET_ACCESS_KEY="${var.aws_secret_access_key}"
-export AWS_DEFAULT_REGION="us-east-1"
-
-
 aws configure set aws_access_key_id "${var.aws_access_key_id}"
 aws configure set aws_secret_access_key "${var.aws_secret_access_key}"
 aws configure set default.region "us-east-1"
